064863f6bcffed6bf19a77525f2a59907bdced8c
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-01-31 22:17:58 -0500

    downloads and file attribute binding in Linux works properly

--- lib/puppet/provider/nexus_artifact/nexus_artifact.rb
+++ lib/puppet/provider/nexus_artifact/nexus_artifact.rb
@@ -1,23 +1,19 @@
 Puppet::Type.type(:nexus_artifact).provide(:nexus_artifact) do
   desc 'Provider for Nexus artifacts'
 
-  #Puppet::Util.which('setfattr')
-
   def ensure
-    if resource[:ensure] == :present
-      return :present if File.exist?(resource[:path])
-    elsif resource[:ensure] == :absent
-      return :absent unless File.exist?(resource[:path])
-    end
-
     @remote_artifacts = get_artifacts(resource)
     @latest_artifact = find_latest_artifact(@remote_artifacts)
 
-    require 'pry'
-    binding.pry
-    attrs = get_attrs(resource[:path])
+    return :absent unless File.exist?(resource[:path])
 
-    if attrs
+    if resource[:ensure] == :present
+      return :present if File.exist?(resource[:path])
+    end
+
+    attrs = get_file_attrs(resource[:path])
+
+    if attrs && attrs['version']
       return attrs['version']
     else
       return :undefined
@@ -30,25 +26,32 @@ Puppet::Type.type(:nexus_artifact).provide(:nexus_artifact) do
     if [:present, :absent].include?(resource[:ensure])
       retval = File.exist?(resource[:path])
     else
-      @remote_artifacts
-
       if resource[:ensure] == :latest
         require 'pry'
         binding.pry
       else
+        require 'pry'
+        binding.pry
 
+        puts 'foo'
       end
     end
 
     return retval
-
   end
 
   def ensure=(should)
-    require 'pry'
-    binding.pry
+    # We always download the latest one by default
+    if [:present, :latest].include?(should)
+      download_asset(resource[:path], @latest_artifact['downloadUrl'])
+      set_file_attrs(resource[:path], @latest_artifact)
 
-    puts 'bar'
+    # The removal case
+    elsif should == :absent
+
+    # The case where we want a specific version
+    else
+    end
   end
 
   private
@@ -117,7 +120,7 @@ Puppet::Type.type(:nexus_artifact).provide(:nexus_artifact) do
   end
 
   def find_latest_artifact(artifacts)
-    artifacts.sort do |a, b|
+    latest_artifact = artifacts.sort do |a, b|
       # If we have two versions, compare them
       if a['version'] && b['version']
         Puppet::Util::Package.versioncmp( a['version'], b['version'] )
@@ -132,6 +135,13 @@ Puppet::Type.type(:nexus_artifact).provide(:nexus_artifact) do
         0
       end
     end.last
+
+    # We don't need all the cruft
+    artifact_version = latest_artifact['version']
+    latest_artifact = latest_artifact['assets'].first
+    latest_artifact['version'] = artifact_version
+
+    return latest_artifact
   end
 
   def get_artifact_items(source, resource)
@@ -170,13 +180,114 @@ Puppet::Type.type(:nexus_artifact).provide(:nexus_artifact) do
     begin
       artifact_items = get_artifact_items(source, resource)
 
-      raise Puppet::Error, 'No remote artifacts found' if artifact_items.empty?
+      raise Puppet::Error, "No remote artifacts found at #{source}" if artifact_items.empty?
 
       return artifact_items
     rescue => e
       # This catches all of the random possible things that can go wrong with
       # HTTP connections.
       raise Puppet::Error, "Could not fetch artifacts from '#{resource[:repository]}/#{resource[:artifact]}' => '#{e}'"
+    end
+  end
+
+  def download_asset(path, url)
+    target_dir = File.dirname(path)
+    target_filename = File.basename(path)
+    temp_file = File.join(target_dir, '.' + target_filename)
+
+    unless File.directory?(target_dir)
+      raise Puppet::Erorr, "Target directory '#{target_dir}' does not exist"
+    end
+
+    begin
+      request, conn = setup_connection(URI(url), resource)
+
+      conn.request(request) do |response|
+        File.open(temp_file, 'wb') do |fh|
+          response.read_body do |chunk|
+            fh.write chunk
+          end
+        end
+      end
+
+      FileUtils.mv(temp_file, path)
+    rescue => e
+      raise(Puppet::Error, "Error when downloading '#{url}' => '#{e}'")
+    end
+  end
+
+  def attr_prefix
+    Puppet.features.root? ? 'trusted' : 'user'
+  end
+
+  def get_file_attrs(path)
+    attrs = nil
+
+    return attrs unless File.exist?(path)
+
+    if Facter.value(:kernel).downcase == 'windows'
+      # TBD
+    else
+      getfattr = Puppet::Util.which('getfattr')
+
+      if getfattr
+        command = [getfattr, '-d', '-m', "'^#{attr_prefix}\.puppet\.simp'", path]
+        output = Puppet::Util::Execution.execute(command, failonfail: false, combine: true)
+
+        if output.exitstatus == 0
+          attrs = Hash[
+            output.lines.grep(/=/).map do |line|
+              k,v = line.strip.split('=')
+              v.gsub!(/(\A"|"\Z)/, '')
+              [k,v]
+            end
+          ]
+        else
+          Puppet.debug("Could not get attributes from #{path} '#{command.join(' ')}' failed: '#{output.to_s}'")
+        end
+      else
+        Puppet.debug('getfattr not found, cannot set extended attributes')
+      end
+    end
+
+    attrs
+  end
+
+  def set_file_attrs(path, asset_info={})
+    attrs = {}
+
+    return attrs unless File.exist?(path)
+
+    if Facter.value(:kernel).downcase == 'windows'
+      # TBD
+    else
+      setfattr = Puppet::Util.which('setfattr')
+
+      if setfattr
+        if asset_info['checksum']
+          asset_info['checksum'].each do |family, value|
+            command = [setfattr, '-n', "#{attr_prefix}.checksum.#{family}", '-v', "#{value}", path]
+
+            output = Puppet::Util::Execution.execute(command, failonfail: false, combine: true)
+
+            unless output.exitstatus == 0
+              Puppet.debug("Could not set attributes on #{path} '#{command.join(' ')}' failed: '#{output.to_s}'")
+            end
+          end
+        end
+
+        if asset_info['version']
+          command = [setfattr, '-n', "#{attr_prefix}.puppet.simp.nexus.version", '-v', "#{asset_info['version']}", path]
+
+          output = Puppet::Util::Execution.execute(command, failonfail: false, combine: true)
+
+          unless output.exitstatus == 0
+            Puppet.debug("Could not set attributes on #{path} '#{command.join(' ')}' failed: '#{output.to_s}'")
+          end
+        end
+      else
+        Puppet.debug('setfattr not found, cannot set extended attributes')
+      end
     end
   end
 end
